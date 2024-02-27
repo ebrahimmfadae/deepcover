7c7bedcb5f46e69c06e7906b1250fcfb81d61115
ebrahimmfadae <ebrahimmfadae@gmail.com>
2024-02-27 23:42:47 +0330

    chore: improve util types

--- src/utils.type.ts
+++ src/utils.type.ts
@@ -5,21 +5,20 @@ export type HardMerge<Destination, Source> = {
 } & Source
 export type CommonKeys<T extends object> = keyof T
 export type AllKeys<T> = T extends unknown ? keyof T : never
-export type Subtract<A, C> = A extends C ? never : A
-export type NonCommonKeys<T extends object> = Subtract<
-  AllKeys<T>,
-  CommonKeys<T>
->
-export type PickType<T, K extends AllKeys<T>> = T extends { [k in K]?: unknown }
-  ? T[K]
-  : undefined
+export type NonCommonKeys<T extends object> = Exclude<AllKeys<T>, CommonKeys<T>>
+export type PickType<T, K extends AllKeys<T>> =
+  T extends Record<string, never>
+    ? never
+    : T extends PartialRecord<K, unknown>
+      ? T[K]
+      : undefined
 export type PickTypeOf<T, K extends string | number | symbol> =
   K extends AllKeys<T> ? PickType<T, K> : never
 export type SoftMerge<T extends object> = MergeIntersection<
   {
-    [k in CommonKeys<T>]: PickTypeOf<T, k>
+    [K in CommonKeys<T>]: PickTypeOf<T, K>
   } & {
-    [k in NonCommonKeys<T>]?: PickTypeOf<T, k>
+    [K in NonCommonKeys<T>]?: PickTypeOf<T, K>
   }
 >
 export type Expandable<T> = T extends Record<string, unknown> ? T : void
@@ -46,7 +45,7 @@ export type Loosen<T> = T extends Primitive
   : {
       [K in keyof T]: T[K] extends Primitive
         ? ToPrimitive<T[K]>
-        : T[K] extends Record<string, unknown> | readonly unknown[]
+        : T[K] extends Record<string, unknown> | unknown[]
           ? Loosen<T[K]>
           : T[K]
     }
