fa1b520a00abc226aa09405961296f356d5c69a7
ebrahimmfadae <ebrahimmfadae@gmail.com>
2024-04-14 15:04:56 +0330

    feat: add deep merge

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -13,7 +13,7 @@ export function cachedGenerator<T extends Generator>(generator: T) {
   } as () => T
 }
 
-export function isPOJO(obj: unknown): obj is object {
+export function isPOJO(obj: unknown): obj is Record<string, any> {
   if (obj === null || typeof obj !== "object") return false
   const prototype = Object.getPrototypeOf(obj)
   return prototype === Object.prototype || prototype === null
@@ -21,4 +21,31 @@ export function isPOJO(obj: unknown): obj is object {
 
 export function typeSafeIsArray<T>(arr: T | T[]): arr is T[] {
   return Array.isArray(arr)
+}
+
+export class Atom<T = unknown> {
+  constructor(public value: T) {}
+}
+
+export function deepMerge(target: any, source: any) {
+  if (source instanceof Atom) return source.value
+  if (target instanceof Atom) return source
+  if (typeof target !== typeof source) return source
+  if (isPOJO(target) && isPOJO(source)) {
+    const ret = { ...target }
+    for (const key in source) {
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        ret[key] = deepMerge(target[key], source[key])
+      }
+    }
+    return ret
+  }
+  if (Array.isArray(target) && Array.isArray(source)) {
+    const ret = [...target]
+    for (let i = 0; i < source.length; i++) {
+      ret[i] = deepMerge(target[i], source[i])
+    }
+    return ret
+  }
+  return source
 }
