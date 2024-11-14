8dc21a4f0bcbb19d42c2eab9e257de7b0b1b8b9b
ebrahimmfadae <ebrahimmfadae@gmail.com>
2024-11-14 13:04:24 +0330

    chore: improve test environment

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -18,8 +18,8 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			src: new URL('./src/', import.meta.url).pathname,
-			test: new URL('./test/', import.meta.url).pathname,
+			'#src': new URL('./src/', import.meta.url).pathname,
+			'#test': new URL('./test/', import.meta.url).pathname,
 		},
 	},
 	plugins: [swc.vite()],
