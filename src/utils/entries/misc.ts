7f5124e4846ba38fcf6a6b1c04db4deb52d2faeb
ebrahimmfadae <ebrahimmfadae@gmail.com>
2025-01-03 14:32:41 +0330

    chore: improve namings

--- src/utils/entries/misc.ts
+++ src/utils/entries/misc.ts
@@ -1,10 +1,14 @@
 import type { UnionToTuple } from '#src/types/union-to-tuple';
 
-export type AllUnionPermutations<T, T2 extends T = T> = T extends unknown
+/**
+ * TODO: Find a better place for this type. Currently it's in `src/utils/entries/misc.ts` and not used.
+ */
+export type UnionToPermutations<T, T2 extends T = T> = T extends unknown
 	? Exclude<T2, T> extends never
 		? readonly [T]
-		: readonly [T, ...AllUnionPermutations<Exclude<T2, T>>]
+		: readonly [T, ...UnionToPermutations<Exclude<T2, T>>]
 	: never;
-export type ValuesAsTuple<T extends Record<string, unknown>, U = UnionToTuple<keyof T>> = {
+
+export type RecordValuesAsTuple<T extends Record<string, unknown>, U = UnionToTuple<keyof T>> = {
 	[K in keyof U]: U[K] extends keyof T ? T[U[K]] : never;
 };
