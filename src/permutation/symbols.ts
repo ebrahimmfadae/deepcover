b6d92cc145712a6370e645423fbe8304ab0b9958
ebrahimmfadae <ebrahimmfadae@gmail.com>
2025-01-14 23:58:55 +0330

    chore: remove unused parallel permutations implementation and tests

--- src/permutation/symbols.ts
+++ src/permutation/symbols.ts
@@ -3,6 +3,3 @@ export type REMOVE = typeof REMOVE;
 
 export const CACHED: unique symbol = Symbol('CACHED');
 export type CACHED = typeof CACHED;
-
-export const NO_ROUTE: unique symbol = Symbol('NO_ROUTE');
-export type NO_ROUTE = typeof NO_ROUTE;
